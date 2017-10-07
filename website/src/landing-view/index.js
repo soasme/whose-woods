cd620db55e7141883077f83be7590e983b123430
soasme <soasme@gmail.com>
2017-10-08 09:50:28 +1300

    setup landing view component.

---
+++ website/src/landing-view/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class LandingView extends Component {
+
+  render() {
+    return (
+      <div className="Landing">
+        <h1>Whose Woods</h1>
+      </div>
+    )
+  }
+}
--- website/src/routes.js
+++ website/src/routes.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Switch, Route  } from 'react-router-dom';
 import App from './App';
 import About from './about-view';
 import NotFound from './404-view';
+import LandingView from './landing-view';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={App} />
+      <Route exact path="/" component={LandingView} />
+      <Route exact path="/app" component={App} />
       <Route path="/about" component={About} />
       <Route path="*" component={NotFound} />
     </Switch>
