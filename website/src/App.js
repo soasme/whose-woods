3edc687955b8a38834dab6e795b8ae714c8c58e1
soasme <soasme@gmail.com>
2017-10-07 17:23:45 +1300

    display record and login component.

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Login from './login/Login';
+import Worklog from './worklog/Worklog';
 import './App.css';
 
 class App extends Component {
@@ -10,9 +12,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Login />
+        <Worklog />
       </div>
     );
   }
--- website/src/worklog/Worklog.js
+++ website/src/worklog/Worklog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Worklog.css';
+import Record from '../record/Record';
 
 export default class Worklog extends Component {
 
@@ -8,6 +9,10 @@ export default class Worklog extends Component {
     this.state = {
       submitting: false,
     }
+    this.records = [
+      {id: 1, content: 'hello world hello world hello world hello world abcdefghigh', created: 1507349064},
+      {id: 2, content: 'hey babe', created: 1507359064},
+    ]
   }
 
   render() {
@@ -17,6 +22,11 @@ export default class Worklog extends Component {
           Click to add ...
         </div>
         <div className="Records">
+        {
+          this.records.map((record, i) =>
+            <Record key={i} record={record} />
+          )
+        }
         </div>
       </div>
     )
