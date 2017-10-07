f31cc6437a722e757cb613bee1a636edbfa3b2c6
soasme <soasme@gmail.com>
2017-10-07 20:21:42 +1300

    add new-record component.

---
+++ website/src/new-record/NewRecord.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './NewRecord.css';
+
+export default class NewRecord extends Component {
+
+  toSubmittingState(e) {
+    e.preventDefault()
+    console.log(e)
+  }
+
+  render() {
+    return (
+      <div className="NewRecord">
+        <div className="Tip">
+          Click to add ...
+        </div>
+        <form className="Panel" onSubmit={this.toSubmittingState}>
+          <div>
+            <textarea className="Content" name="content"></textarea>
+          </div>
+          <div>
+            <input className="Submit" type="submit" value="Submit"/>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
