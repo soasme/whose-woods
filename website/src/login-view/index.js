827942713630d9dd07d55f873077a9503fe23f6c
soasme <soasme@gmail.com>
2017-10-08 21:46:28 +1300

    set username & password to store.

--- website/src/login-view/index.js
+++ website/src/login-view/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { bindActionCreators  } from 'redux'
 import { connect  } from 'react-redux'
 import {
-  requestAuthorization
+  requestAuthorization,
+  provideUsername,
+  providePassword,
 } from '../modules/authorization';
 
 const LoginView = props => (
@@ -11,24 +13,31 @@ const LoginView = props => (
       <h1 className="App-title">Login to Whose Woods</h1>
     </header>
     <div>
-      Username: <input type="text" name="username" />
+      Username: <input type="text" name="username"
+        onChange={(e) => props.provideUsername(e.target.value)} />
     </div>
     <div>
-      Password: <input type="password" name="password" />
+      Password: <input type="password" name="password"
+        onChange={(e) => props.providePassword(e.target.value)} />
     </div>
     <div>
-      <button onClick={props.requestAuthorization}>Login</button>
+      <button onClick={props.requestAuthorization}
+        disabled={props.isAuthorizing}>Login</button>
     </div>
   </div>
 )
 
 const mapStateToProps = state => ({
   isAuthorizing: state.authorization.isAuthorizing,
+  credentialUsername: state.authorization.credentialUsername,
+  credentialPassword: state.authorization.credentialPassword,
   userProfile: state.authorization.userProfile
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  requestAuthorization
+  requestAuthorization,
+  provideUsername,
+  providePassword
 }, dispatch)
 
 export default connect(
--- website/src/modules/authorization.js
+++ website/src/modules/authorization.js
@@ -1,11 +1,17 @@
 export const AUTH_UNAUTHORIZED = '/authorization/unauthorized';
+export const AUTH_CRED_USERNAME_PROVIDED = '/authorization/cred_username_provided';
+export const AUTH_CRED_PASSWORD_PROVIDED = '/authorization/cred_password_provided';
 export const AUTH_SUBMITTED = '/authorization/submitted';
 export const AUTH_VERIFYING = '/authorization/verifying';
 export const AUTH_AUTHORIZED = '/authorization/authorized';
 export const AUTH_ERROR = '/authorization/error';
 
 const initialState = {
+  error: '',
   isAuthorizing: false,
+  isAuthorized: false,
+  credentialUsername: '',
+  credentialPassword: '',
   userProfile: {
     id: 0
   },
@@ -18,6 +24,16 @@ export default (state = initialState, action) => {
         ...state,
         isAuthorizing: true
       }
+    case AUTH_CRED_USERNAME_PROVIDED:
+      return {
+        ...state,
+        credentialUsername: action.data
+      }
+    case AUTH_CRED_PASSWORD_PROVIDED:
+      return {
+        ...state,
+        credentialPassword: action.data
+      }
     default:
       return state
   }
@@ -27,6 +43,24 @@ export const requestAuthorization = () => {
   return dispatch => {
     dispatch({
       type: AUTH_SUBMITTED
+    })
+  }
+}
+
+export const provideUsername = (data) => {
+  return dispatch => {
+    dispatch({
+      type: AUTH_CRED_USERNAME_PROVIDED,
+      data: data
+    })
+  }
+}
+
+export const providePassword = (data) => {
+  return dispatch => {
+    dispatch({
+      type: AUTH_CRED_PASSWORD_PROVIDED,
+      data: data
     })
   }
 }
