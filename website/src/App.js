42330ef9643054a5226c83e8bccb9ed2200c2a02
soasme <soasme@gmail.com>
2017-10-08 14:44:50 +1300

    design landing page.

--- website/src/App.js
+++ website/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title"><WorkspacePicker /> Woods</h1>
+          <h1 className="App-title"><WorkspacePicker /></h1>
         </header>
         <Worklog />
       </div>
--- website/src/landing-view/index.js
+++ website/src/landing-view/index.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { Link  } from 'react-router-dom';
+import './style.css';
 
 export default class LandingView extends Component {
 
   render() {
     return (
-      <div className="Landing">
-        <h1>Whose Woods</h1>
+      <div className="Landing App">
+        <header className="App-header">
+          <h1 className="App-title">Whose Woods</h1>
+        </header>
+        <div className="Entry">
+          <h2>Track what you have done</h2>
+          <h3>Always know you are on the right track.</h3>
+          <Link to="/app" className="LaunchApp">
+          <button>
+            Launch App
+          </button>
+          </Link>
+        </div>
       </div>
     )
   }
