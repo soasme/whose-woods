51d5edba175d5c0b2d8e8c19fc0355a598d11bf2
soasme <soasme@gmail.com>
2017-10-08 15:51:46 +1300

    add workspace view component.

---
+++ website/src/workspace-view/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import NewRecord from '../new-record/NewRecord';
+import WorkspacePicker from '../workspace-picker/WorkspacePicker';
+import Record from '../record/Record';
+import './style.css';
+
+export default class Workspace extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      submitting: false,
+    }
+    this.records = [
+      {id: 1, content: 'hello world hello world hello world hello world abcdefghigh', created: 1507349064},
+      {id: 2, content: 'hey babe', created: 1507359064},
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title"><WorkspacePicker /></h1>
+        </header>
+        <div className="Worklog">
+          <NewRecord />
+          <div className="Records">
+          {
+            this.records.map((record, i) =>
+              <Record key={i} record={record} />
+            )
+          }
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
