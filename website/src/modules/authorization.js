3458fe9d9514de163f56534d99b1524eacdfa895
soasme <soasme@gmail.com>
2017-10-09 21:55:40 +1300

    define requestProfile action.

--- website/src/modules/authorization.js
+++ website/src/modules/authorization.js
@@ -1,10 +1,12 @@
-export const AUTH_UNAUTHORIZED = '/authorization/unauthorized';
-export const AUTH_CRED_USERNAME_PROVIDED = '/authorization/cred_username_provided';
-export const AUTH_CRED_PASSWORD_PROVIDED = '/authorization/cred_password_provided';
-export const AUTH_SUBMITTED = '/authorization/submitted';
-export const AUTH_VERIFYING = '/authorization/verifying';
-export const AUTH_AUTHORIZED = '/authorization/authorized';
-export const AUTH_ERROR = '/authorization/error';
+export const AUTH_CRED_USERNAME_PROVIDED = '/authorization/cred_username_provided'
+export const AUTH_CRED_PASSWORD_PROVIDED = '/authorization/cred_password_provided'
+export const AUTH_SUBMITTED = '/authorization/submitted'
+export const AUTH_AUTHORIZED = '/authorization/authorized'
+export const AUTH_ERROR = '/authorization/error'
+
+export const LOADING_PROFILE = '/authorization/profile/load/doing'
+export const LOADED_PROFILE = '/authorization/profile/load/done'
+export const LOAD_ERROR = '/authorization/profile/load/failure'
 
 const initialState = {
   error: '',
@@ -12,18 +14,17 @@ const initialState = {
   isAuthorized: false,
   credentialUsername: '',
   credentialPassword: '',
+  isLoadingProfile: false,
+  isLoadedProfile: false,
+  loadProfileError: '',
   userProfile: {
-    id: 0
+    id: 0,
+    username: ''
   },
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case AUTH_SUBMITTED:
-      return {
-        ...state,
-        isAuthorizing: true
-      }
     case AUTH_CRED_USERNAME_PROVIDED:
       return {
         ...state,
@@ -33,6 +34,11 @@ export default (state = initialState, action) => {
       return {
         ...state,
         credentialPassword: action.data
+      }
+    case AUTH_SUBMITTED:
+      return {
+        ...state,
+        isAuthorizing: true
       }
     case AUTH_AUTHORIZED:
       return {
@@ -48,8 +54,83 @@ export default (state = initialState, action) => {
         isAuthorized: false,
         error: action.data.message
       }
+    case LOADING_PROFILE:
+      return {
+        ...state,
+        isLoadingProfile: true,
+        isLoadedProfile: false,
+      }
+    case LOADED_PROFILE:
+      return {
+        ...state,
+        isAuthorizing: false,
+        isAuthorized: true,
+        isLoadingProfile: false,
+        isLoadedProfile: true,
+        userProfile: action.data
+      }
+    case LOAD_ERROR:
+      return {
+        ...state,
+        isLoadingProfile: false,
+        isLoadedProfile: false,
+        loadProfileError: action.data.message
+      }
     default:
       return state
+  }
+}
+
+export const requestProfile = () => {
+  return async (dispatch, getState) => {
+    dispatch({
+      type: LOADING_PROFILE
+    })
+
+    const token = localStorage.getItem('token') || ''
+
+    try {
+      const res = await fetch('http://127.0.0.1:3001/profile', {
+        headers: {
+          'Authorization': token ? `Bearer ${token}` : ''
+        }
+      })
+
+      if (res.status >= 400) {
+        dispatch({
+          type: LOAD_ERROR,
+          data: {
+            message: 'Unsuccessful request.',
+            status: res.status
+          }
+        })
+        return
+      }
+
+      try {
+        const data = await res.json()
+        dispatch({
+          type: LOADED_PROFILE,
+          data: data
+        })
+      } catch (error) {
+        dispatch({
+          type: LOAD_ERROR,
+          data: {
+            message: "Bad Data"
+          }
+        })
+      }
+    } catch (error) {
+      dispatch({
+        type: LOAD_ERROR,
+        data: {
+          message: error.toString()
+        }
+      })
+
+    }
+
   }
 }
 
