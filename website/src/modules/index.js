1d74e59112128ff9b952e42658bcfcbbbe48db44
soasme <soasme@gmail.com>
2017-10-10 23:02:53 +1300

    install form reducer.

--- website/src/modules/index.js
+++ website/src/modules/index.js
@@ -1,10 +1,12 @@
 import { combineReducers  } from 'redux'
 import { routerReducer  } from 'react-router-redux'
+import { reducer as formReducer  } from 'redux-form'
 import authorization from './authorization'
 import workspace from './workspace'
 
 export default combineReducers({
   router: routerReducer,
+  form: formReducer,
   authorization,
   workspace
 })
