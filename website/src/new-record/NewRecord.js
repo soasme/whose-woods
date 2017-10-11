2b6da93294158a7c6cc2cc3b8cdfb5b21ad88c2b
soasme <soasme@gmail.com>
2017-10-11 21:24:40 +1300

    bind stat ewith NewRecord.

--- website/src/new-record/NewRecord.js
+++ website/src/new-record/NewRecord.js
@@ -1,20 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { addRecord } from '../modules/record'
 import './NewRecord.css';
 
-export default class NewRecord extends Component {
-
-  toSubmittingState(e) {
-    e.preventDefault()
-    console.log(e)
-  }
-
-  render() {
-    return (
-      <div className="NewRecord">
-        <div className="Tip">
-          Click to add ...
-        </div>
+const NewRecord = props => {
+  return (
+    <div className="NewRecord"
+      onClick={() => props.addRecord()}>
+      <div className="Tip">
+      { props.isAddingRecord ? 'Adding ...' : 'Click to add' }
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+const mapStateToProps = state => ({
+  isAddingRecord: state.record.isAdding,
+  isAddedRecord: state.record.isAdded,
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  addRecord,
+}, dispatch)
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(NewRecord)
