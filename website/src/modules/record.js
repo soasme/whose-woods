58ebe7b3c8d1e2578334b44613e4d6fd3d707687
soasme <soasme@gmail.com>
2017-10-11 21:23:33 +1300

    add fetch list.

--- website/src/modules/record.js
+++ website/src/modules/record.js
@@ -1,17 +1,40 @@
 export const RC_FETCHLIST_LOADING = '/record/fetchList/loading'
 export const RC_FETCHLIST_LOADED = '/record/fetchList/loaded'
+export const RC_RECORD_ADDING = '/record/adding'
+export const RC_RECORD_ADDED = '/record/added'
 
 const initState = {
+  fetchStatus: 'loading',
   entities: [],
-  fetchStatus: {}
+  isAdding: false,
+  isAdded: false,
 }
 
 export default (state = initState, action) => {
   switch (action.type) {
+    case RC_FETCHLIST_LOADING:
+      return {
+        ...state,
+        fetchStatus: 'doing',
+      }
     case RC_FETCHLIST_LOADED:
       return {
         ...state,
+        fetchStatus: 'done',
         entities: action.data
+      }
+    case RC_RECORD_ADDING:
+      return {
+        ...state,
+        isAdding: true,
+        isAdded: false,
+      }
+    case RC_RECORD_ADDED:
+      return {
+        ...state,
+        isAdding: false,
+        isAdded: true,
+        fetchStatus: 'waiting',
       }
     default:
       return state
@@ -20,6 +43,9 @@ export default (state = initState, action) => {
 
 export const fetchRecords = () => {
   return async (dispatch, getState) => {
+    dispatch({
+      type: RC_FETCHLIST_LOADING,
+    })
     const res = await fetch("http://127.0.0.1:3001/records", {
       headers: {
         'Authorization': `Bearer ${getState().authorization.token}`
@@ -28,6 +54,27 @@ export const fetchRecords = () => {
     const data = await res.json()
     dispatch({
       type: RC_FETCHLIST_LOADED,
+      data: data
+    })
+  }
+}
+
+export const addRecord = () => {
+  return async (dispatch, getState) => {
+    dispatch({
+      type: RC_RECORD_ADDING
+    })
+    const res = await fetch("http://127.0.0.1:3001/records", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${getState().authorization.token}`
+      },
+      body: JSON.stringify({workspace_id: 1, content: '', created_at: parseInt(Date.now() / 1000, 10)})
+    })
+    const data = await res.json()
+    dispatch({
+      type: RC_RECORD_ADDED,
       data: data
     })
   }
