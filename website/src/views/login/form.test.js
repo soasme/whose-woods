48c89d278256057c851a9976f8af7ae87f4be3fc
soasme <soasme@gmail.com>
2017-10-13 22:35:57 +1300

    add form element test.

--- website/src/views/login/form.test.js
+++ website/src/views/login/form.test.js
@@ -1,4 +1,5 @@
 import React  from 'react';
+import { expect  } from 'chai';
 import { mount, shallow  } from 'enzyme'
 import { LoginForm }from './form'
 import { Field } from 'redux-form'
@@ -9,8 +10,24 @@ describe('login form', () => {
   describe('fields', () => {
     it('should render username & password field', () => {
       const wrapper = shallow(<LoginForm />)
-      // one for username and another for password
-      expect(wrapper.find(Field)).toHaveLength(2)
+      expect(wrapper.find(Field)).to.have.length(2)
+      expect(wrapper.find({name: 'username'}).exists()).to.be.true
+      expect(wrapper.find({name: 'password'}).exists()).to.be.true
+    })
+
+    it('should render submit button', () => {
+      const wrapper = shallow(<LoginForm />)
+      expect(wrapper.find({type: 'submit'}).exists()).to.be.true
+    })
+
+    it('should render error if we provide error', () => {
+      const wrapper = shallow(<LoginForm error='Huston, we have a problem.' />)
+      expect(wrapper.find({className: 'error'}).exists()).to.be.true
+    })
+
+    it('should not render error if there is no error', () => {
+      const wrapper = shallow(<LoginForm />)
+      expect(wrapper.find({className: 'error'}).exists()).to.be.false
     })
   })
 })
