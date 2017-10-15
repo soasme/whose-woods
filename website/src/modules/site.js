dc0795abf032573cca5a6f91a3449685130b51f0
soasme <soasme@gmail.com>
2017-10-15 23:09:20 +1300

    define default function for site.

--- website/src/modules/site.js
+++ website/src/modules/site.js
@@ -3,3 +3,10 @@ const initialState = {
   api: 'http://127.0.0.1:3001',
   loggedInEndpoint: '/workspace',
 }
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
