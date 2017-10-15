9c17bca32b99da50572bb9ff1fb79b344588a8b7
soasme <soasme@gmail.com>
2017-10-15 20:33:02 +1300

    init entity module.

---
+++ website/src/modules/entities.js
@@ -0,0 +1,17 @@
+const initialState = {
+  profile: {
+    status: 'unknown',
+    error: null,
+    entity: {},
+  },
+  records: {
+    entities: {},
+    errors: {},
+    statuses: {},
+  },
+  workspaces: {
+    entities: {},
+    errors: {},
+    statuses: {},
+  }
+}
